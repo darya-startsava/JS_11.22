56c7d4039fb6a59e73fd68b2c506e4123b502dc6
darya-startsava <daryastartsava@gmail.com>
2022-11-06 17:45:03 +0100

    [task_2]: add solution

---
+++ task2.js
@@ -0,0 +1,11 @@
+function selectFromInterval(array, firstNumber, secondNumber) {
+  if (!Array.isArray(array) || array.some((item) => typeof item !== 'number' || isNaN(item))) {
+    throw new Error();
+  }
+  if (typeof firstNumber !== 'number' || isNaN(firstNumber) || typeof secondNumber !== 'number' || isNaN(secondNumber)) {
+    throw new Error();
+  }
+  const START = Math.min(firstNumber, secondNumber);
+  const END = Math.max(firstNumber, secondNumber);
+  return array.filter((item) => item >= START && item <= END);
+}
