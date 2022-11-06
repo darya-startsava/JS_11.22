9abfc89307e83d650f9e0ece0ce17283442d03bd
darya-startsava <daryastartsava@gmail.com>
2022-11-06 17:45:13 +0100

    [task_3]: add solution

---
+++ task3.js
@@ -0,0 +1,10 @@
+function createIterable(from, to) {
+  if (typeof from !== 'number' || isNaN(from) || typeof to !== 'number' || isNaN(to) || to <= from) {
+    throw new Error();
+  }
+  const obj = {};
+  obj[Symbol.iterator] = function* () {
+    for (let i = from; i <= to; ++i) yield i;
+  };
+  return obj;
+}
