f0b1d25e3eac0070162f1dd85ee7dbdd4801dc7c
darya-startsava <daryastartsava@gmail.com>
2022-11-07 01:55:32 +0100

    [task_1]: fix solution

--- task1.js
+++ task1.js
@@ -1,17 +1,86 @@
 function makeDeepCopy(obj) {
+  function goDeeperMap(map, copyMap) {
+    for ([key, value] of map) {
+      if (typeof value !== 'object' || value === null) {
+        copyMap.set(key, value);
+        continue;
+      }
+      if (value instanceof Map) {
+        const deeperCopyMap = new Map();
+        copyMap.set(key, deeperCopyMap);
+        return goDeeperMap(value, deeperCopyMap);
+      }
+      if (value instanceof Set) {
+        const deeperCopySet = new Set();
+        copyMap.set(key, deeperCopyset);
+        return goDeeperSet(value, deeperCopySet);
+      }
+      if (Array.isArray(value)) {
+        const deeperCopyArray = [];
+        copyMap.set(key, deeperCopyArray);
+        return goDeeper(value, deeperCopyArray);
+      }
+      const deeperCopyObject = {};
+      copyMap.set(key, deeperCopyObject);
+      return goDeeper(value, deeperCopyObject);
+    }
+  }
+
+  function goDeeperSet(set, copySet) {
+    for (item of set) {
+      if (typeof item !== 'object' || item === null) {
+        copySet.add(item);
+        continue;
+      }
+      if (item instanceof Map) {
+        const deeperCopyMap = new Map();
+        copySet.add(deeperCopyMap);
+        return goDeeperMap(item, deeperCopyMap);
+      }
+      if (item instanceof Set) {
+        const deeperCopySet = new Set();
+        copySet.add(deeperCopySet);
+        return goDeeperSet(item, deeperCopySet);
+      }
+      if (Array.isArray(item)) {
+        const deeperCopyArray = [];
+        copySet.add(deeperCopyArray);
+        return goDeeper(item, deeperCopyArray);
+      }
+      const deeperCopyObject = {};
+      copySet.add(deeperCopyObject);
+      return goDeeper(item, deeperCopyObject);
+    }
+  }
+
+  function goDeeper(object, copy) {
+    Object.keys(object).forEach((key) => {
+      if (typeof object[key] !== 'object' || object[key] === null) {
+        copy[key] = object[key];
+        return;
+      }
+      if (Array.isArray(object[key])) {
+        copy[key] = [];
+        return goDeeper(object[key], copy[key]);
+      }
+      if (object[key] instanceof Map) {
+        copy[key] = new Map();
+        return goDeeperMap(object[key], copy[key]);
+      }
+      if (object[key] instanceof Set) {
+        copy[key] = new Set();
+        return goDeeperSet(object[key], copy[key]);
+      }
+      copy[key] = {};
+      return goDeeper(object[key], copy[key]);
+    });
+  }
+
   if (obj === null || typeof obj !== 'object') {
     throw new Error();
   }
-  const deepCopy = Array.isArray(obj) ? [...obj] : { ...obj };
+  const deepCopy = {};
 
-  function goDeeper(copy) {
-    Object.keys(copy).forEach((key) => {
-      if (typeof copy[key] === 'object' && copy[key] !== null) {
-        copy[key] = Array.isArray(copy[key]) ? [...copy[key]] : { ...copy[key] };
-        return goDeeper(copy[key]);
-      }
-    });
-  }
-  goDeeper(deepCopy);
+  goDeeper(obj, deepCopy);
   return deepCopy;
 }
