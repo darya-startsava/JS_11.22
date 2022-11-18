e11dabaa29c0d6155af2d25e4aa239beec6fdccd
darya-startsava <daryastartsava@gmail.com>
2022-11-18 01:15:45 +0100

    [task_1]: update solution
    change properties to private

--- index.js
+++ index.js
@@ -29,62 +29,75 @@ function isValidNumberFromInterwal(number, min = 0, max = Infinity) {
 }
 
 class Stack {
+  #size = 0;
+  #value;
+  #prevElement;
+  #maxSize;
+
   constructor(maxSize = 10) {
-    if (
-      typeof maxSize !== 'number' ||
-      !isFinite(maxSize) ||
-      isNaN(maxSize) ||
-      maxSize <= 0
-    ) {
-      throw new Error('Invalid limit value');
-    }
     this.maxSize = maxSize;
-    this.size = 0;
-    this.value;
-    this.prevElement;
   }
 
   push(elem) {
-    if (this.size === this.maxSize) {
+    if (this.#size === this.maxSize) {
       throw new Error('Limit exceeded');
     }
 
-    if (this.size > 0) {
-      this.prevElement = { value: this.value, prevElement: this.prevElement };
+    if (this.#size > 0) {
+      this.#prevElement = { value: this.#value, prevElement: this.#prevElement };
     }
-    this.value = elem;
-    this.size++;
+    this.#value = elem;
+    this.#size++;
   }
 
   pop() {
-    if (this.size === 0) {
+    if (this.#size === 0) {
       throw new Error('Empty stack');
     }
-    const elem = this.value;
-    this.size--;
-    this.value = this.prevElement?.value;
-    this.prevElement = this.prevElement?.prevElement;
+    const elem = this.#value;
+    this.#size--;
+    this.#value = this.#prevElement?.value;
+    this.#prevElement = this.#prevElement?.prevElement;
     return elem;
   }
 
   peek() {
-    if (this.size === 0) {
+    if (this.#size === 0) {
       return null;
     }
-    return this.value;
+    return this.#value;
   }
 
   isEmpty() {
-    if (this.size === 0) {
+    if (this.#size === 0) {
       return true;
     }
     return false;
   }
 
+  get value() {
+    return this.#value;
+  }
+
+  get prevElement() {
+    return this.#prevElement;
+  }
+
+  get maxSize() {
+    return this.#maxSize;
+  }
+
+  set maxSize(value) {
+    if (typeof value !== 'number' || !isFinite(value) || isNaN(value) || value < 0) {
+      throw new Error('Invalid limit value');
+    }
+    this.#maxSize = value;
+  }
+
   toArray() {
     const array = [];
     let elem = this;
-    for (let i = this.size; i !== 0; i--) {
+    for (let i = this.#size; i !== 0; i--) {
       array.push(elem.value);
       elem = elem.prevElement;
     }
@@ -93,12 +106,11 @@ class Stack {
 
   static fromIterable(iterable) {
     checkIterability(iterable);
-    const stack = new Stack();
+    const stack = new Stack(0);
     for (const item of iterable) {
       stack.maxSize++;
       stack.push(item);
     }
-    stack.maxSize = stack.size;
     return stack;
   }
 }
