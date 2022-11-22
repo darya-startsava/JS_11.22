f3b5c81b0e61149d34c5657c436c5e46fe630c82
darya-startsava <daryastartsava@gmail.com>
2022-11-22 13:55:32 +0100

    feat: add basic calculation logic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function toReversePolishNotationExpression(string) {
   const arr = string.split(/\s/).filter((i) => i && i !== ' ');
+
   const priority = { '+': 1, '-': 1, '*': 2, '/': 2, '(': 0 };
   const stack = [];
   const expression = [];
@@ -64,3 +65,77 @@ function reversePolishNotation(expression) {
   return stack[0];
 }
 
+let currentNumber = '0';
+let expression = '';
+const currentNumberElement = document.getElementById('current-number');
+const expressionElement = document.getElementById('expression');
+
+function enterNumber(number) {
+  if (currentNumber === '0') {
+    if (number === '.') {
+      currentNumber += number;
+    } else if (number !== '0' && number !== '00') {
+      currentNumber = number;
+    }
+  } else {
+    if (number === '.' && currentNumber.includes('.')) {
+      return;
+    }
+    currentNumber = currentNumber + number;
+  }
+  currentNumberElement.innerHTML = currentNumber;
+}
+
+function enterOperand(operand) {
+  expression += currentNumber + operand;
+  expressionElement.innerHTML = expression;
+  currentNumber = '0';
+  currentNumberElement.innerHTML = currentNumber;
+}
+
+function calculate() {
+  expression += currentNumber;
+  expressionElement.innerHTML = expression;
+  currentNumber = '0';
+  currentNumberElement.innerHTML = reversePolishNotation(
+    toReversePolishNotationExpression(expression)
+  );
+}
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+const doubleZero = document.getElementById('double-zero');
+const point = document.getElementById('point');
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const multiply = document.getElementById('multiply');
+const divide = document.getElementById('divide');
+const openBracket = document.getElementById('open-bracket');
+const closeBracket = document.getElementById('close-bracket');
+const equals = document.getElementById('equals');
+
+one.addEventListener('click', () => enterNumber('1'));
+two.addEventListener('click', () => enterNumber('2'));
+three.addEventListener('click', () => enterNumber('3'));
+four.addEventListener('click', () => enterNumber('4'));
+five.addEventListener('click', () => enterNumber('5'));
+six.addEventListener('click', () => enterNumber('6'));
+seven.addEventListener('click', () => enterNumber('7'));
+eight.addEventListener('click', () => enterNumber('8'));
+nine.addEventListener('click', () => enterNumber('9'));
+zero.addEventListener('click', () => enterNumber('0'));
+doubleZero.addEventListener('click', () => enterNumber('00'));
+point.addEventListener('click', () => enterNumber('.'));
+plus.addEventListener('click', () => enterOperand(' + '));
+minus.addEventListener('click', () => enterOperand(' - '));
+multiply.addEventListener('click', () => enterOperand(' * '));
+divide.addEventListener('click', () => enterOperand(' / '));
+equals.addEventListener('click', () => calculate());
